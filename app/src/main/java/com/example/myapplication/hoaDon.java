60d77e7b3fc38b0f8ab791ab4cc9130f2b332b73
python3112 <78737006+python3112@users.noreply.github.com>
2023-03-24 06:16:58 +0700

    hoangvhhph29660 ui Hoa Don + Kieu chu

--- app/src/main/java/com/example/myapplication/hoaDon.java
+++ app/src/main/java/com/example/myapplication/hoaDon.java
@@ -1,15 +1,35 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class hoaDon extends AppCompatActivity {
+import com.example.myapplication.AdapterManager.hoaDonAdapter;
+import com.example.myapplication.ObjectManager.hoaDonObj;
 
+import java.util.ArrayList;
+
+public class hoaDon extends AppCompatActivity {
+        RecyclerView recycleView_hoadon_activity;
+        hoaDonAdapter hoadon_adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoa_don);
         getSupportActionBar().setTitle("Quản lý Hóa đơn");
+        recycleView_hoadon_activity = findViewById(R.id.recycleView_hoadon_activity);
+
+
+
+    }
+    private ArrayList<hoaDonObj> data_test(){
+        ArrayList<hoaDonObj> arrayList = new ArrayList<>();
+        arrayList.add(new hoaDonObj("sdsds" , "p3j3j" , "chưa thanh toán" ,"200000" , "20/12/2023" , "Fb88"));
+        arrayList.add(new hoaDonObj("ndsjfd" , "p3j3j" , "chưa thanh toán" ,"800000" , "22/12/2023" , "Fb88"));
+        arrayList.add(new hoaDonObj("dsbdbsa" , "p3j3j" , "chưa thanh toán" ,"500000" , "25/12/2023" , "Fb88"));
+        arrayList.add(new hoaDonObj("dsnds" , "p3j3j" , "chưa thanh toán" ,"600000" , "10/12/2023" , "Fb88"));
+        arrayList.add(new hoaDonObj("dsandsan" , "p3j3j" , "chưa thanh toán" ,"300000" , "30/12/2023" , "Fb88"));
+        return arrayList;
     }
 }
