e61421c5fd92c9010f3a817e033e8e7698d8a631
Devol <nguyenducbinhduong@gmail.com>
2023-03-30 14:22:56 +0700

    Duong update

--- app/src/main/java/com/example/myapplication/AdapterManager/loaiPhongAdapter.java
+++ app/src/main/java/com/example/myapplication/AdapterManager/loaiPhongAdapter.java
@@ -1,9 +1,16 @@
 package com.example.myapplication.AdapterManager;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Filter;
 import android.widget.Filterable;
 
@@ -13,6 +20,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myapplication.InterfaceManager.sendLoaiPhong;
 import com.example.myapplication.ObjectManager.loaiPhongObj;
 import com.example.myapplication.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.List;
 
@@ -47,7 +55,17 @@ public class loaiPhongAdapter extends RecyclerView.Adapter<loaiPhongAdapter.loai
         if (items == null){
             return;
         }
-        //todo...
+        //gán view cho từng phần tử
+        holder.edtMaLoaiPhong.setText(items.getMaLoai());
+        holder.edtTenLoaiPhong.setText(items.getTenLoaiPhong());
+
+        //onclick Sửa hiện dialog
+        holder.btnSua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSuaLoaiPhongDialog(Gravity.CENTER);
+            }
+        });
     }
 
     @Override
@@ -58,11 +76,58 @@ public class loaiPhongAdapter extends RecyclerView.Adapter<loaiPhongAdapter.loai
         return 0;
     }
     public final class loaiPhongViewHolder extends RecyclerView.ViewHolder {
-        //todo...
+        //khai báo các phần tử
+        TextInputEditText edtMaLoaiPhong,edtTenLoaiPhong;
+        Button btnSua;
         public loaiPhongViewHolder(@NonNull View itemView) {
             super(itemView);
-            //todo...
+            //gán view cho từng phần tử
+            edtMaLoaiPhong = itemView.findViewById(R.id.edtMaLoaiPhong);
+            edtTenLoaiPhong = itemView.findViewById(R.id.edtTenLoaiPhong);
+            btnSua = itemView.findViewById(R.id.btnSua);
         }
+    }
+
+    private void openSuaLoaiPhongDialog(int gravity){
+        final Dialog dialog = new Dialog(mContext);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_update_loai_phong);
+
+        Window window = dialog.getWindow();
+        if(window == null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttribute = window.getAttributes();
+        windowAttribute.gravity =gravity;
+        window.setAttributes(windowAttribute);
+
+        //Xử lý click ra ngoài dialog
+        if(Gravity.BOTTOM == gravity){
+            dialog.setCancelable(true);
+        } else {
+            dialog.setCancelable(false);
+        }
+        Button btnHuy =dialog.findViewById(R.id.btnHuy);
+        Button btnSua =dialog.findViewById(R.id.btnSua);
+
+        //Xử lý nút trong Dialog
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnSua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loaiPhongObj loaiPhong = new loaiPhongObj();
+            }
+        });
+
+        dialog.show();
     }
     @Override
     public Filter getFilter() {
